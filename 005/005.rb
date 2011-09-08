c7bb79ede430994b4633bca5385dcffea1b8be3e
Charles Copprell <chowdy@gmail.com>
2011-09-07 17:54:39 -0700

    Cleaned up 005.

--- 005/005.rb
+++ 005/005.rb
@@ -1,24 +1,15 @@
-answer = 0
-
-# test 1 to 10
-# MAX = 10
 MAX = 20
 i = MAX
 while true
 
-  divided_evenly = 0
-
   (1..MAX).each do |j|
     break if not (i % j).zero?
-    divided_evenly = j
-  end
-
-  if divided_evenly == MAX
-    answer = i
-    break
+    
+    if j == MAX
+      puts i
+      exit
+    end
   end
 
   i += MAX
 end
-
-puts answer
