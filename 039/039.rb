f7d788d81330f1e0c0ceff4ed43208da74c08607
Charles Copprell <chowdy@gmail.com>
2011-09-08 15:01:29 -0700

    039 done

---
+++ 039/039.rb
@@ -0,0 +1,28 @@
+def num_sols(p)
+  sols = 0
+
+  ((p/3)..p).each do |c|
+    (1..c).each do |a|
+      b = p - (a + c)
+
+      next if b <= 0
+
+      if a * a + b * b == c * c
+        sols += 1 
+      end
+
+    end
+  end
+
+  return sols
+end
+
+answer, max = 0, 0
+12.upto(1000).each do |p|
+  if (buf = num_sols(p)) > max
+    max = buf
+    answer = p
+  end
+end
+puts answer
+puts 840
