6ede7b9c6b2350fbc5b169f91c6e4b63b5a830ba
Charles Copprell <chowdy@gmail.com>
2011-09-07 17:17:50 -0700

    Super duper cheating improvement. Ah well.

--- 003/003.rb
+++ 003/003.rb
@@ -1,16 +1,2 @@
 require 'mathn'
-
-input = 600_851_475_143
-answer = 0
-
-Prime.instance.each do |i|
-  break if i > input
-
-  div = input / i
-  next if div > div.floor
-
-  input /= i
-  answer = i
-end
-
-puts answer
+puts 600_851_475_143.prime_division.flatten.max
