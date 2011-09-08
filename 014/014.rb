767d91d19e78e381fdbe336b5b5def5f1559eeac
Charles Copprell <chowdy@gmail.com>
2011-09-08 11:47:29 -0700

    Made 014 uglier, but I am much happier with its speed (~3 sec from ~30sec)

--- 014/014.rb
+++ 014/014.rb
@@ -1,10 +1,20 @@
+@seen = {}
 answer, max = 0, 0
 (2...1_000_000).each do |i|
   count = 0
   buf = i
-  while (buf = (buf.even? ? buf / 2 : 3 * buf + 1)) != 1
+
+  while buf != 1
+    if not (seen = @seen[buf]).nil?
+      count += seen
+      break
+    end
+
+    buf = buf.even? ? buf / 2 : 3 * buf + 1
     count += 1
   end
+
+  @seen[i] = count
 
   if count > max
     max = count
